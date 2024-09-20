e1bbe441438954bc2b777a37691c7fca79c7f792
Manish kumar <tejnarayan1919@gmail.com>
2024-09-20 23:10:08 +0530

    working on header section

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,2 +1,17 @@
-"use client"
-import { useState, useEffect } from "react"
+"use client";
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Button } from "./ui/button";
+import {
+  Menu,
+  Coins,
+  Leaf,
+  Search,
+  Bell,
+  User,
+  ChevronDown,
+  LogIn,
+  LogOut,
+} from "lucide-react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem,DropdownMenuTrigger} from "./ui/dropdown-menu";
