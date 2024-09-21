727be1236162bbe99f11ba2955e5ee1e56edddb8
Manish kumar <tejnarayan1919@gmail.com>
2024-09-21 13:12:06 +0530

    creating user modal and header , but still in complete header

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,4 +14,80 @@ import {
   LogIn,
   LogOut,
 } from "lucide-react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem,DropdownMenuTrigger} from "./ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
+import { Web3Auth } from "@web3auth/modal";
+import { CHAIN_NAMESPACES, IProvider, WEB3AUTH_NETWORK } from "@web3auth/base";
+import { EthereumPrivateKeyProvider } from "@web3auth/ethereum-provider";
+import { createUser } from "@/utils/db/actions";
+// import {useMediaQuery} from ''
+const clientId =
+  "BJKdDFkNtkWX87XqkuWrDu4rbkSvWyQZ5lswS0ucINxxcN0inRVW8zzKAywPPzgiOHP7_3PcfFwfpvcQvSdaLRs";
+const chainConfig = {
+  chainNamespace: CHAIN_NAMESPACES.EIP155,
+  chainId: "0xaa36a7",
+  rpcTarget: "https://rpc.ankr.com/eth_sepolia",
+  displayName: "Sepolia Testnet",
+  blockExplorerUrl: "https://sepolia.etherscan.io",
+  ticker: "ETH",
+  tickerName: "Ethereum",
+  logo: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
+};
+
+const privateKeyProvider = new EthereumPrivateKeyProvider({
+  config: { chainConfig },
+});
+const web3auth = new Web3Auth({
+  clientId,
+  web3AuthNetwork: WEB3AUTH_NETWORK.TESTNET, // Changed from SAPPHIRE_MAINNET to TESTNET
+  privateKeyProvider,
+});
+
+interface HeaderProps {
+  onMenuClick: () => void;
+  totalEarnings: number;
+}
+
+export default function Header({ onMenuClick, totalEarnings }: HeaderProps) {
+  const [provider, setProvider] = useState<IProvider | null>(null);
+  const [loggedin, setLoggedin] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [userInfo, setUserInfo] = useState<any>(null);
+  const pathName = usePathname();
+  const [notification, setNotification] = useState<Notification[]>([]);
+  const [balance, setBalance] = useState(0);
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        await web3auth.initModal();
+
+        if (web3auth.provider) {
+          setProvider(web3auth.provider);
+          setLoggedin(true);
+
+          const user = await web3auth.getUserInfo();
+          setUserInfo(user);
+
+          if (user.email) {
+            localStorage.setItem("userEmail", user.email);
+            try {
+              await createUser(user.email, user.name || "Anonymous user");
+            } catch (error) {
+              console.log("Error creating user", error);
+            }
+          }
+        }
+      } catch (error) {
+        console.error("Error initializing Web3Auth:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    init();
+  }, []);
+}
